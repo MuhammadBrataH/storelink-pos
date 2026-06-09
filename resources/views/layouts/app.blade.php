6ca1b2cdcbd3db75673127db653cc5236eea5dce
wyandhanupapoy <wyandhanupapoy@gmail.com>
2026-06-09 20:50:50 +0700

    Remove analytics tab and fix CSS import order

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,16 +104,7 @@
                         </div>
                     </a>
                 </div>
-                <!-- Analytics -->
-                <a class="flex justify-center items-center w-full py-3 text-gray-400 hover:text-gray-600 transition-colors"
-                    href="#">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewbox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
-                    </svg>
-                </a>
+
                 <!-- Users -->
                 <a class="flex justify-center items-center w-full py-3 text-gray-400 hover:text-gray-600 transition-colors group relative {{ request()->routeIs('users.*') ? 'bg-blue-50 text-blue-600 rounded-lg' : '' }}"
                     href="{{ route('users.index') }}">
