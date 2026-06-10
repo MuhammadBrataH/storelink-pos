220d08abf505454fe8396cd2fec80e07926b6789
MuhammadBrataH <muhammadbrata06@gmail.com>
2026-06-10 07:17:08 +0700

    Fix Vercel read-only crash by moving bootstrap cache to /tmp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,6 +38,7 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL')) {
         "$storagePath/framework/testing",
         "$storagePath/framework/views",
         "$storagePath/logs",
+        "/tmp/bootstrap/cache",
     ];
 
     foreach ($directories as $dir) {
@@ -45,6 +46,13 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL')) {
             mkdir($dir, 0777, true);
         }
     }
+
+    // Override Bootstrap Cache paths
+    $_ENV['APP_SERVICES_CACHE'] = $_SERVER['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+    $_ENV['APP_PACKAGES_CACHE'] = $_SERVER['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+    $_ENV['APP_CONFIG_CACHE'] = $_SERVER['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+    $_ENV['APP_ROUTES_CACHE'] = $_SERVER['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes-v7.php';
+    $_ENV['APP_EVENTS_CACHE'] = $_SERVER['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
 }
 
 return $app;
