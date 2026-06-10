ba79d450bd9441914bea5468aa414e39ccb11df0
MuhammadBrataH <muhammadbrata06@gmail.com>
2026-06-10 07:13:15 +0700

    Add try-catch in api/index.php to debug 500 error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Vercel PHP Fatal Error</h1>";
+    echo "<p><strong>Message:</strong> " . $e->getMessage() . "</p>";
+    echo "<p><strong>File:</strong> " . $e->getFile() . " on line " . $e->getLine() . "</p>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
